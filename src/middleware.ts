d78914ae70c670ac2fb128955a21c060fc26a938
Otavio Tampellini <tampelliniotavio@gmail.com>
2024-12-06 21:04:55 -0300

    feat: redirect if is logged in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,20 @@
 import { sequence } from "astro:middleware";
 import { middleware as localeMiddleware } from "astro:i18n";
+import type { MiddlewareHandler } from "astro";
+import { getAuth } from "./lib/auth";
+import { useTranslations } from "./i18n/utils";
+
+const authMiddleware: MiddlewareHandler = (context, next) => {
+    if (
+        context.url.pathname.includes("/login") &&
+        getAuth(context.cookies) !== undefined
+    ) {
+        const { lang } = useTranslations(context.url);
+        return context.redirect("/" + lang);
+    }
+
+    return next();
+};
 
 export const onRequest = sequence(
     localeMiddleware({
@@ -7,4 +22,5 @@ export const onRequest = sequence(
         prefixDefaultLocale: true,
         redirectToDefaultLocale: true,
     }),
+    authMiddleware,
 );
