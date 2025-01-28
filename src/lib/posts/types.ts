4196abd6ef375662821fe2e0b4a8a765ed054a03
Otavio Tampellini <tampelliniotavio@gmail.com>
2025-01-28 16:36:04 -0300

    chore(posts): add post fields

--- src/lib/posts/types.ts
+++ src/lib/posts/types.ts
@@ -2,6 +2,8 @@ export interface IPost {
     xid: string;
     post: string;
     createdBy: string;
+    likeCount: number;
     createdAt: string;
     updatedAt: string;
+    deletedAt: string | null;
 }
